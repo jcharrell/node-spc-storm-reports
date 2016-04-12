5be1975777f9878e892199e2015ff2f54bc7c2a3
Chris Harrell <chris.harrell@sq1.com>
2016-04-12 12:50:46 -0500

    Add initial test

---
+++ test/reports-request.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const reportsRequest = require('./../src/lib/reports-request');
+
+describe('Get SPC Reports', function getReportsSuccessDescribe() {
+	let response;
+
+	before(function* getReportsSuccessBefore() {
+		response = yield reportsRequest('2016-04-04');
+	});
+
+	it('should return with a HTTP status of 200', function noRightsCode() {
+		expect(response.statusCode).to.exist;
+		expect(response.statusCode).to.equal(200);
+	});
+
+	it("should respond with a valid response body", function () {
+		expect(response.body).to.exist;
+	});
+});

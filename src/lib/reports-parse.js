155f1c1961081502664672123eb75faa6528db42
Chris Harrell <chris.harrell@sq1.com>
2016-04-12 21:59:40 -0500

    Wrap logic in a generator

--- src/lib/reports-parse.js
+++ src/lib/reports-parse.js
@@ -1,17 +1,17 @@
 'use strict';
 
+const co = require('co');
 const Promise = require('bluebird');
 const parseProduct = require('./product-parse');
 
-module.exports = function praseReports(data) {
+module.exports = function parseReports(data) {
 	// Return the products defined in the optional `options` object or return all.
 	// http://www.spc.noaa.gov/climo/data/nglsr/data/rpts/160406.log
-	try {
+	return co(function* spcRequest() {
 		var results = parseProduct('tornado', data);
 
 		return Promise.resolve(results);
-	} catch(err) {
-		throw err;
-	}
-
+	}).catch(function errorHandler(err) {
+		throw (err);
+	});
 };

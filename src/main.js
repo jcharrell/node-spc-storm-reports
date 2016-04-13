de37741404efc79cf01abddd25eb188210eca634
Chris Harrell <chris.harrell@sq1.com>
2016-04-12 22:21:00 -0500

    Add the entry module

---
+++ src/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Regular expression to validate the requested date format
+const request = require('./lib/reports-request');
+const parse = require('./lib/reports-parse');
+const Promise = require('bluebird');
+const co = require('co');
+
+// Default options, which determine the data attributes to request and return
+// TODO: Find a better naming convention that works with the SPC defined column titles
+const defaultOptions = {
+	tornado: true,
+    hail: true,
+	wndg: true,
+	gust: false,
+	blizzard: false,
+	freezingRain: false,
+	heavySnow: false,
+	iceStorm: false,
+	sleet: false,
+	snow: false,
+	wildfire: false
+};
+
+module.exports = function requestReports(date, options) {
+    co(function* gatherReports() {
+        let response = yield request(date);
+        let reports = yield parse(response.body, defaultOptions);
+		console.log(reports.tornado);
+    }).catch(function errorHandler(err) {
+        console.error(err.stack);
+        throw(err);
+    });
+};

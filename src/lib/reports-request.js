140329bcd28e7b691360dca6fb167d6f9875495e
Chris Harrell <chris.harrell@sq1.com>
2016-04-18 08:10:44 -0500

    Utilize node http and drop `request` dependency

--- src/lib/reports-request.js
+++ src/lib/reports-request.js
@@ -1,22 +1,27 @@
 'use strict';
 
-const baseUrl = 'http://www.spc.noaa.gov/climo/data/nglsr/data/rpts/';
 const dateRegex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
-const Promise = require('bluebird');
-const request = Promise.promisifyAll(require('request'));
+const request = require('./request');
 const co = require('co');
 
 module.exports = function reportsRequest(date) {
+
 	return co(function* spcRequest() {
 		if (!dateRegex.test(date)) {
 			throw new Error("'date' must be a valid date format.");
 		}
 
+		// Utilize the supplied date to produce the SPC log date format
 		date = date.replace(/\d{2}(\d{2})-(\d{2})-(\d{2})/, '$1$2$3');
 
-		let response =  yield request.getAsync(`${baseUrl}${date}.log`);
+		const requestOptions = {
+			host: 'www.spc.noaa.gov',
+			path: `/climo/data/nglsr/data/rpts/${date}.log`
+		};
 
-		if(response.statusCode !== 200) {
+		let response = yield request(requestOptions);
+
+		if (response.statusCode !== 200) {
 			throw new Error('Invalid response from the SPC.')
 		}
 
